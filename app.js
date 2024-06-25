11a18eca8b7c7c121de1b389b400447a52c9c757
Aerokinesis <alexwduong@gmail.com>
2024-06-24 23:33:20 -1000

    Added logic to playGame

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-console.log("Hello World!");
-
-
-// Randomly generates a hand. Rock, Paper, or Scissors
-function getRandomInt() {
+function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3 + 1);
     if (randomChoice == 1) {
         return "rock";
@@ -13,46 +9,64 @@ function getRandomInt() {
     }
   }
 
-let randomChoice = getRandomInt()
-console.log(randomChoice);
-
-// Get user input. Valid: rock, paper, or scissors
 
 function getHumanChoice () {
     let userInput = prompt("rock, paper, or scissors");
     return userInput.toLowerCase();
 }
 
-let userChoice = getHumanChoice();
-console.log(userChoice);
-
-
-
-
-
-
-
-
-
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
+    
+    let countRound = 0;
 
-    if (humanChoice == computerChoice) {
-        return `You tied`;
+        function playRound(humanChoice, computerChoice) {
+
+            console.log(`Computer choice: ${computerChoice}`);
+            console.log(`Your choice: ${humanChoice}`)
+            if (humanChoice == computerChoice) {
+                countRound++;
+                
+                console.log(`You both tied. Your score: ${humanScore}. Computer score ${computerScore}`);
+                console.log(`Current round ${countRound}`);
+            }
+
+            if ((humanChoice == "rock" && computerChoice == "scissors") || 
+            (humanChoice == "paper" && computerChoice == "rock") || 
+            (humanChoice == "scissors" && computerChoice == "paper"))
+            {
+                humanScore++;
+                countRound++;
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+                console.log(`Your score ${humanScore} - Computer score ${computerScore} - Current round ${countRound}`);
+            } else if ((computerChoice == "rock" && humanChoice == "scissors") || 
+            (computerChoice == "paper" && humanChoice == "rock") || 
+            (computerChoice == "scissors" && humanChoice =="paper")){
+                computerScore++;
+                countRound++;
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                console.log(`Your score ${humanScore} - Computer score ${computerScore} - Current round ${countRound}`);
+            }
+        }
+
+        
+    
+
+    for (let i = 1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
+}
 
-    if ((humanChoice == "rock" && computerChoice == "scissors") || 
-    (humanChoice == "paper" && computerChoice == "rock") || 
-    (humanChoice == "scissors" && computerChoice == "paper"))
-    {
-        humanScore++;
-        return `You win! ${humanChoice} beats ${computerChoice}`;
-    } else if ((computerChoice == "rock" && humanChoice == "scissors") || (computerChoice == "paper" && humanChoice == "rock") || (computerChoice == "scissors" && humanChoice =="paper")){
-        computerScore++;
-        return `You lose! ${computerChoice} beats ${humanChoice}`;
-    }
-} 
+playGame();
 
-console.log(playRound(userChoice, randomChoice));
+if (humanScore == computerScore) {
+    console.log("Game Tied!") ;
+} else if (humanScore > computerScore) {
+    console.log("You won!");
+} else {
+    console.log("Computer won!");
+}
